50ce4b553ac7b20f716fdddeeb6fdcc52c507ccf
MintPro11 <63410040@go.buu.ac.th>
2022-12-05 15:10:01 +0700

    first commit

---
+++ phpjson_0040_p33.php
@@ -0,0 +1,13 @@
+<?php
+$connect = mysqli_connect("localhost","root","","maindb");
+$filename = "employee.json";
+$data = file_get_contents($filename);
+$array = json_decode($data, true);
+
+foreach($array as $row) {   
+$sql = "INSERT INTO employee (id, name, gender, designation) 
+VALUES('".$row["id"]."','".$row["name"]."','".$row["gender"]."','".$row["designation"]."')";
+mysqli_query($connect,$sql);
+}
+echo "Employee data inserted";
+?>
